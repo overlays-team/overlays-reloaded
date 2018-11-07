ffead2a19f3a99953e0181f800cbeb34ddf1de7f
Shuya Fuchigami <shuyafuchigami@shmcbkpro17.local>
2018-11-07 15:16:35 +0100

    level menu prototype

---
+++ Assets/LevelSelector.cs
@@ -0,0 +1,15 @@
+﻿
+using UnityEngine;
+
+public class LevelSelector : MonoBehaviour {
+
+    //public SceneFelder fader;
+
+    public void Select(string levelName)
+    {
+        //fader.FadeTo(levelName);
+    }
+
+
+
+}
