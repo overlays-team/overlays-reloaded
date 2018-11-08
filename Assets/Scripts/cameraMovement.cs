0ba7c5cf07bebf94c092bce23554a7e4ea49dbe6
Konstantin Regenhardt <IceCWiener@users.noreply.github.com>
2018-11-08 14:39:29 +0100

    Button-Funktionen & erster Slider

---
+++ Assets/Scripts/cameraMovement.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class cameraMovement : MonoBehaviour {
+
+    public GameObject lookAtTarget;
+    public Slider sliderX;
+
+	// Use this for initialization
+	void Start () {
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        transform.LookAt(lookAtTarget.transform);
+	}
+
+}
