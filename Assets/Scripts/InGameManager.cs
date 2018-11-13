be4c2f34b63d2c6aec90dd66967bb8ea66cdb35d
Aleksandr Sandalov <alex-sandr11@yandex.ru>
2018-11-13 21:02:01 +0100

    first version

---
+++ Assets/Scripts/InGameManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class InGameManager : MonoBehaviour {
+
+    GameData gameData;
+    GoalBlock goalBlock;
+    Scene thisScene;
+
+    // Use this for initialization
+    void Start () {
+
+        thisScene = SceneManager.GetActiveScene();
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+        if (goalBlock.isGoalAchieved)
+        {
+            List<LevelData> levels = gameData.getLevels();
+            foreach (LevelData level in levels)
+            {
+                if (level.getName == thisScene.name)
+                {
+                    level.isCompleted = true;
+                }
+            }
+            SceneManager.LoadScene("LevelComplete"); //or otherwise 
+        }
+		
+	}
+}
