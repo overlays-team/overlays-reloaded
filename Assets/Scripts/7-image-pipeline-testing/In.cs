51547135a1d45d4099f5c94abc20c5357050f460
Master-Bernardo <benito4747@live.com>
2018-11-16 10:42:54 +0100

    initial commit

---
+++ Assets/Scripts/7-image-pipeline-testing/In.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class In : MonoBehaviour {
+
+    public Texture2D inputImage;
+    public Image debugImage;
+
+    //referenz auf den nächsten Node
+    public ImageProcessor imageProcessor;
+
+    void Start()
+    {
+        //convert the picture into the RGBA32 texture format
+        Texture2D outputImage = new Texture2D(inputImage.width, inputImage.height, TextureFormat.RGBA32, false);
+        for (int y = 0; y < outputImage.height; y++)
+        {
+            for (int x = 0; x < outputImage.width; x++)
+            {
+                outputImage.SetPixel(x, y, inputImage.GetPixel(x, y));
+            }
+
+        }
+        outputImage.Apply();
+
+        imageProcessor.SetInputImage(outputImage);
+        debugImage.sprite = Sprite.Create(outputImage, new Rect(0, 0, outputImage.width, outputImage.height), new Vector2(0.5f, 0.5f));
+    }
+
+}
---
+++ Assets/Scripts/7-image-pipeline-testing/Out.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Out : MonoBehaviour {
+
+    Texture2D inputImage;
+    public Image debugImage;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+ 
+    }
+
+    public void SetInputImage(Texture2D inputImage)
+    {
+        this.inputImage = inputImage;
+        debugImage.sprite = Sprite.Create(inputImage, new Rect(0, 0, inputImage.width, inputImage.height), new Vector2(0.5f, 0.5f));
+    }
+}
