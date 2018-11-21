5b4cd70f8383cdba4e312f00a62beb1908189225
Konstantin Regenhardt <IceCWiener@users.noreply.github.com>
2018-11-21 14:46:45 +0100

    add newFilterScripts

--- Assets/Scripts/BlockObjects/RedFilter.cs
+++ Assets/Scripts/BlockObjects/RedFilter.cs
@@ -2,15 +2,46 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class RedFilter : MonoBehaviour {
+public class RedFilter : BlockObject
+{
+    [SerializeField]
+    private LaserInput laserInput;
+    private Laser inputLaser;
+    [SerializeField]
+    private LaserOutput laserOutput;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    protected override void Start()
+    {
+        base.Start();
+        laserOutput.active = false;
+    }
+
+    protected override void Update()
+    {
+        base.Update();
+
+        if (inputLasers.Count > 0)
+        {
+            foreach (Laser laser in inputLasers)
+            {
+                //check if one of our input Lasers hits the input
+                if (Vector3.Angle(laser.laserOutput.forward, laserInput.transform.forward) < 1)
+                {
+                    laserOutput.active = true;
+                    inputLaser = laser;
+                    return;
+                }
+                else
+                {
+                    laserOutput.active = false;
+                    inputLaser = null;
+                }
+            }
+        }
+        else
+        {
+            laserOutput.active = false;
+            inputLaser = null;
+        }
+    }
 }
