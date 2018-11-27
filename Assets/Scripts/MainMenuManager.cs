82dab9e14e51eeea9703d91aa49d065ef9fd3a2a
Konstantin Regenhardt <IceCWiener@users.noreply.github.com>
2018-11-27 17:19:00 +0100

    Change switched Button functions to MainMenuManager

---
+++ Assets/Scripts/MainMenuManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuManager : MonoBehaviour {
+
+    [Header("Insert Prefabs")]
+    public GameObject mainMenu;
+    public GameObject levelSelect;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    //Functions of the main menu buttons
+    public void ExitApp()
+    {
+        Application.Quit();
+    }
+
+    public void MainMenuLevelSelectSwitch()
+    {
+        if (mainMenu.activeSelf)
+        {
+            mainMenu.SetActive(false);
+            levelSelect.SetActive(true);
+        } else if (levelSelect.activeSelf)
+        {
+            mainMenu.SetActive(true);
+            levelSelect.SetActive(false);
+        }
+    }
+}
