cea88c11dc74dc421fa0ea1d6befef4cf0d04307
Konstantin Regenhardt <IceCWiener@users.noreply.github.com>
2018-11-28 17:06:46 +0100

    Add MainMenuManagerand merged menues

---
+++ Assets/MainMenuManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class MainMenuManager : MonoBehaviour {
+
+    public GameObject MainMenu;
+    public GameObject LevelSelect;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void ExitApp()
+    {
+        Application.Quit();
+    }
+
+    public void MainMenulevelSelectSwitch()
+    {
+        if (MainMenu.activeSelf)
+        {
+            MainMenu.SetActive(false);
+            LevelSelect.SetActive(true);
+        } else if (LevelSelect.activeSelf)
+        {
+            MainMenu.SetActive(true);
+            LevelSelect.SetActive(false);
+        }
+    }
+}
