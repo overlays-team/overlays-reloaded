ca00146d3a4ba1d699ac24c993fd298ba949862f
kainsteffen <tranduykhanh1996@yahoo.de>
2018-12-17 15:42:10 +0100

    Add logo

--- Assets/Scripts/MainMenu/MainMenuManager.cs
+++ Assets/Scripts/MainMenu/MainMenuManager.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class MainMenuManager : MonoBehaviour {
 
+    public GameObject logo;
     public GameObject mainMenu;
     public GameObject modeSelect;
     public GameObject levelSelect;
@@ -28,18 +29,21 @@ public class MainMenuManager : MonoBehaviour {
         modeSelect.SetActive(true);
         levelSelect.SetActive(false);
         mainMenu.SetActive(false);
+        logo.SetActive(false);
     }
 
     public void ShowLevelSelect()
     {
         levelSelect.SetActive(true);
         mainMenu.SetActive(false);
+        logo.SetActive(false);
         modeSelect.SetActive(false);
     }
 
     public void ShowMainMenu()
     {
         mainMenu.SetActive(true);
+        logo.SetActive(true);
         modeSelect.SetActive(false);
         levelSelect.SetActive(false);
     }
