831fcbd8455fa26bda526ba99c7f21644161b1d5
kainsteffen <tranduykhanh1996@yahoo.de>
2018-12-20 15:42:59 +0100

    Fix level selector

--- Assets/LevelSelector.cs
+++ Assets/LevelSelector.cs
@@ -52,12 +52,6 @@ public class LevelSelector : MonoBehaviour
         GameDataEditor.Instance.data.levels.Add(new LevelData("LEVEL7", false));
         GameDataEditor.Instance.data.levels.Add(new LevelData("LEVEL8", false));
     }
-    */
-
-
-    private void CreateTestLevelState()
-    {
-        Debug.Log("こんにちは、CreateTestLevelState()");
 
    /* private void LoadTestLevelStateFromFile()
     {
