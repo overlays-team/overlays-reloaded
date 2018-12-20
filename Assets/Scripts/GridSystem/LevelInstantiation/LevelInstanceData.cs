b4a206e96ab19b3d45a7bee1e8de0d9ed3946887
Konstantin Regenhardt <IceCWiener@users.noreply.github.com>
2018-12-20 17:40:17 +0100

    Add levelInstantiator Scripts

---
+++ Assets/Scripts/GridSystem/LevelInstantiation/LevelInstanceData.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class LevelInstanceData {
+
+    public int I00;
+    public int I01;
+    public int I10;
+    public int I11;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/GridSystem/LevelInstantiation/LevelInstantiator.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+[System.Serializable]
+public class LevelInstantiator : MonoBehaviour {
+
+    public string dataFileName;
+    LevelInstanceData myLevel;
+    //Example for 2x2 Array
+    private int I00;
+    private int I01;
+    private int I10;
+    private int I11;
+    //private string[,] array;
+
+    // Use this for initialization
+    void Start () {
+        //Filling the objects that becomes the json
+        myLevel = new LevelInstanceData{};
+        LoadData();
+        Debug.Log("00: "+myLevel.I00+" 01: "+myLevel.I01);
+    }
+
+    public void LoadData()
+    {
+        if (File.Exists(getFilePath()))
+        {
+            string dataAsJson = File.ReadAllText(getFilePath());
+            print("DataAsJson: "+dataAsJson);
+            myLevel = JsonUtility.FromJson<LevelInstanceData>(dataAsJson);
+        }
+        else
+        {
+            Debug.Log("Please save correct " + dataFileName + " to " + Application.persistentDataPath);
+        }
+    }
+
+    public string getFilePath()
+    {
+        return Application.persistentDataPath + dataFileName;
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
