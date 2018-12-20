0cfefd9dd7e420387fff05129a43cc6b3c6dc3d9
kainsteffen <tranduykhanh1996@yahoo.de>
2018-12-20 02:19:59 +0100

    Refactor project structure

---
+++ Assets/Scripts/SceneFader.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class SceneFader : MonoBehaviour {
+
+    public float animationSpeed = 1; //the lager the value, the slower the fading
+    public Image img;
+    public AnimationCurve curve;
+
+    private void Start()
+    {
+        //StartCoroutine(FadeIn());
+    }
+
+    public void FadeTo(string scene)
+    {
+        StartCoroutine(FadeOut(scene));
+    }
+ 
+    IEnumerator FadeIn()
+    {
+        float t = 1f;
+
+        while (t > 0)
+        {
+            t -= Time.deltaTime * animationSpeed; //t decrease by every single frame
+            float a = curve.Evaluate(t);
+            img.color = new Color(0f, 0f, 0f, a);
+            yield return 0; //break. wait unti the next frame and continue
+        }
+    }
+
+    IEnumerator FadeOut(string scene)
+    {
+        float t = 0f;
+
+        while (t < 1)
+        {
+            t += Time.deltaTime * animationSpeed; //t increase by every single frame
+            float a = curve.Evaluate(t);
+            img.color = new Color(0f, 0f, 0f, a);
+            yield return 0; //break. wait unti the next frame and continue
+        }
+
+        SceneManager.LoadScene(scene);
+    }
+}
