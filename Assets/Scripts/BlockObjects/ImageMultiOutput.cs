32f3657bb8270978d6eb78a3d296a39b0edd2d88
Kat <kate.rizhakova@gmail.com>
2019-01-16 12:59:55 +0100

    changed to old version of code by ImageMultiOutput

--- Assets/Scripts/BlockObjects/ImageMultiOutput.cs
+++ Assets/Scripts/BlockObjects/ImageMultiOutput.cs
@@ -6,21 +6,13 @@ using System.IO;
 
 public class ImageMultiOutput : ImageOutput
 {
-    private Color correctcolor = new Color(1, 0, 0);
-    private Color incorrectcolor = new Color(0, 1, 0);
-    private Color checkingcolor = new Color(1, 1, 0);
-    [SerializeField]
-    private Color framecolor = new Color(0, 0, 0); //if laser output is active
-    [SerializeField]
-    private float intensity = 1;
 
     protected override void ImageOutputUpdate()
     {
         if (lasersChanged)
         {
             imageCorrect = false;
-            frame.material.SetColor("_Color", incorrectcolor);
-            framecolor = incorrectcolor;
+            frame.SetColors(Color.red, Color.red);
             StopCoroutine("ImageCheckingEnumerator");
             imageCheckingState = ImageCheckingState.NoImage;
             StopImageProcessing();
@@ -39,13 +31,11 @@ public class ImageMultiOutput : ImageOutput
                 inputImage1 = activeLasers[0].inputLaser.image;
                 inputImage2 = activeLasers[1].inputLaser.image;
                 StartImageProcessing();
-                ChangeFrameMaterial(framecolor, intensity);
             }
             else if (activeLasers.Count == 1)
             {
                 debugImage.sprite = Sprite.Create(activeLasers[0].inputLaser.image, new Rect(0, 0, activeLasers[0].inputLaser.image.width, activeLasers[0].inputLaser.image.height), new Vector2(0.5f, 0.5f));
                 CheckIfImageIsCorrect(activeLasers[0].inputLaser.image);
-                ChangeFrameMaterial(framecolor, intensity);
             }
             else
             {
@@ -53,10 +43,8 @@ public class ImageMultiOutput : ImageOutput
                 inputImage2 = null;
 
                 debugImage.sprite = Sprite.Create(goalImage, new Rect(0, 0, goalImage.width, goalImage.height), new Vector2(0.5f, 0.5f));
-                frame.material.SetColor("_Color", incorrectcolor);
-                framecolor = incorrectcolor;
+                frame.SetColors(Color.red, Color.red);
                 imageCorrect = false;
-                ChangeFrameMaterial(framecolor, intensity);
             }
         }
 
@@ -68,8 +56,7 @@ public class ImageMultiOutput : ImageOutput
 
                 //start checking if our image is processed
                 imageCheckingState = ImageCheckingState.Checking;
-                frame.material.SetColor("_Color", checkingcolor);
-                framecolor = checkingcolor;
+                frame.SetColors(Color.yellow, Color.yellow);
                 CheckIfImageIsCorrect(outputImage);
                 imageProcessingState = ImageProcessingState.Displaying;
             }
@@ -82,12 +69,11 @@ public class ImageMultiOutput : ImageOutput
                 imageCheckingState = ImageCheckingState.Displaying;
                 if (imageCorrect)
                 {
-                    ChangeFrameMaterial(framecolor, intensity);
+                    frame.SetColors(Color.green, Color.green);
                 }
                 else
                 {
-                    frame.material.SetColor("_Color", incorrectcolor);
-                    ChangeFrameMaterial(framecolor, intensity);
+                    frame.SetColors(Color.red, Color.red);
                 }
             }
         }
@@ -109,21 +95,4 @@ public class ImageMultiOutput : ImageOutput
                         );
     }
 
-    void ChangeFrameMaterial(Color emissioncolor, float intesity)
-    {
-        if (this.transform.childCount == 0)
-        {
-            return;
-        }
-        foreach (Transform child in graphics.transform)
-        {
-            GameObject gochild = child.gameObject;
-            LineRenderer renderer = gochild.GetComponent<LineRenderer>();
-            if (renderer != null)
-            {
-                renderer.material.SetColor("_EmissionColor", emissioncolor * intesity);
-
-            }
-        }
-    }
 }
