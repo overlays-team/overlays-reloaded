636afa9726893735d3f40105c6737211b247e7c7
Master-Bernardo <benito4747@live.com>
2019-01-17 09:37:29 +0100

    added mask and barthel block
    barthel block not yet ready

---
+++ Assets/Scripts/BlockObjects/BarthelCombine2.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BarthelCombine2 : BlockObject
+{
+
+    //takes 2 lasers as Input and gives one as output
+
+    float image1Weight = 1;
+    float image2Weight = 1;
+
+
+    protected override void Update()
+    {
+        base.Update();
+
+        if (lasersChanged)
+        {
+            imageProcessingState = ImageProcessingState.NoImage;
+            if (laserInputs[0].active && laserInputs[1].active)
+            {
+                inputImage1 = laserInputs[0].inputLaser.image;
+                inputImage2 = laserInputs[1].inputLaser.image;
+                Grow();
+                StartImageProcessing();
+
+            }
+            else
+            {
+                inputImage1 = null;
+                inputImage2 = null;
+                Shrink();
+                StopImageProcessing();
+            }
+
+        }
+
+        UpdateOutputImageDisplayAndSendThroughLaser();
+    }
+
+    protected override void StartImageProcessing()
+    {
+        base.StartImageProcessing();
+        if (inputImage1.width != inputImage2.width) Debug.Log("different resolutions of the images we want to screen/overlay!");
+    }
+
+    protected override Color ProcessPixel(int x, int y)
+    {
+        return new Color(
+                        1 - (1 - (inputImage1.GetPixel(x, y).a * image1Weight * inputImage1.GetPixel(x, y).r)) * (1 - (inputImage2.GetPixel(x, y).a * image2Weight * inputImage2.GetPixel(x, y).r)) / 1,
+                        1 - (1 - (inputImage1.GetPixel(x, y).a * image1Weight * inputImage1.GetPixel(x, y).g)) * (1 - (inputImage2.GetPixel(x, y).a * image2Weight * inputImage2.GetPixel(x, y).g)) / 1,
+                        1 - (1 - (inputImage1.GetPixel(x, y).a * image1Weight * inputImage1.GetPixel(x, y).b)) * (1 - (inputImage2.GetPixel(x, y).a * image2Weight * inputImage2.GetPixel(x, y).b)) / 1,
+                        Mathf.Max(inputImage1.GetPixel(x, y).a, inputImage2.GetPixel(x, y).a)
+                        );
+    }
+
+    //gets called by the slider in the detsiled node view, updates the weight of the two images
+    public void UpdateImageWeight(Slider slider)
+    {
+        Debug.Log(slider.value);
+        image1Weight = slider.value * 2;
+        image2Weight = (1 - slider.value) * 2;
+        if (inputImage1 != null && inputImage2 != null) StartImageProcessing();
+    }
+
+
+}
---
+++ Assets/Scripts/BlockObjects/Mask.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Mask : BlockObject
+{
+    [Tooltip("assign the mask here - the mask takes the r value for alpha blending")]
+    public Texture2D mask;
+
+    protected override void Start()
+    {
+        base.Start();
+        blockImage.GetComponent<Image>().sprite = Sprite.Create(mask, new Rect(0, 0, mask.width, mask.height), new Vector2(0.5f, 0.5f));
+    }
+
+    // Update is called once per frame
+    protected override void Update()
+    {
+        base.Update();
+
+        if (lasersChanged)
+        {
+            imageProcessingState = ImageProcessingState.NoImage;
+            if (laserInputs[0].active)
+            {
+                inputImage1 = laserInputs[0].inputLaser.image;
+                Grow();
+                StartImageProcessing();
+            }
+            else
+            {
+                inputImage1 = null;
+                Shrink();
+                StopImageProcessing();
+            }
+        }
+
+        UpdateOutputImageDisplayAndSendThroughLaser();
+    }
+
+    protected override Color ProcessPixel(int x, int y)
+    {
+        return new Color(
+            Mathf.Abs(inputImage1.GetPixel(x, y).r),
+            Mathf.Abs(inputImage1.GetPixel(x, y).g),
+            Mathf.Abs(inputImage1.GetPixel(x, y).b), 
+            mask.GetPixel(x, y).r);
+    }
+}
