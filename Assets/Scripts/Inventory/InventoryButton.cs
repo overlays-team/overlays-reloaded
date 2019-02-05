bea6038866050161ffeb92009469d07fffc21e50
Master-Bernardo <benito4747@live.com>
2019-02-05 11:54:34 +0100

    adapted inventory size
    now looking good on 16:9, 16:10 and ipad size

--- Assets/Scripts/Inventory/InventoryButton.cs
+++ Assets/Scripts/Inventory/InventoryButton.cs
@@ -2,11 +2,25 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 //simple button, which tells the inventory when it is pressed
 public class InventoryButton : MonoBehaviour, IPointerDownHandler
 {
     public Inventory inventory;
+
+    [Tooltip("this automaticly changes the inventory icon size on tablet")]
+    public LayoutElement layoutElement;
+
+    private void Start()
+    {
+        if (Screen.height *1f/ Screen.width < 1.5)
+        {
+            layoutElement.minHeight = 120;
+            layoutElement.minWidth = 120;
+        }
+
+    }
 
     public void OnPointerDown(PointerEventData eventData)
     {
