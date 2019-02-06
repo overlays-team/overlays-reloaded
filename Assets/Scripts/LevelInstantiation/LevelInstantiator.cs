3681086c9fa0ca487e4864719fd8e4e33467b67b
Master-Bernardo <benito4747@live.com>
2019-02-06 17:09:46 +0100

    added more pictures to time attack
    lets test

--- Assets/Scripts/LevelInstantiation/LevelInstantiator.cs
+++ Assets/Scripts/LevelInstantiation/LevelInstantiator.cs
@@ -79,7 +79,7 @@ public class LevelInstantiator : MonoBehaviour
 
     private void assignPics()
     {
-        int numOfFolders = 16; //Needs to be the number of folders in PictureSets + 1
+        int numOfFolders = 35; //Needs to be the number of folders in PictureSets + 1
         randomFolder = UnityEngine.Random.Range(1, numOfFolders);
         int temp = randomFolder;
         _sourceImage1 = getPicA();
