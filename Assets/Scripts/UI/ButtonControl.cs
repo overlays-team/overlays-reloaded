44b928f7f86276009b8092e6ce1006a6d0e738ae
kainsteffen <tranduykhanh1996@yahoo.de>
2019-02-15 19:30:47 +0100

    Change project folder structure

---
+++ Assets/Scripts/UI/ButtonControl.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class ButtonControl : MonoBehaviour {
+   
+    public void Next()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void Retry()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+    public void Home(){
+        SceneManager.LoadScene("Home");
+    }
+
+    public void Select(string Level)
+    {
+        SceneManager.LoadScene(Level);
+    }
+}
